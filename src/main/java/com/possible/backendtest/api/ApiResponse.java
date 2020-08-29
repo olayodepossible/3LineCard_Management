9ecb0d603ec705fb25ffc8adf73c016dc22a055c
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-08-29 10:57:14 +0100

    code refactored

--- src/main/java/com/possible/backendtest/api/ApiResponse.java
+++ src/main/java/com/possible/backendtest/api/ApiResponse.java
@@ -4,18 +4,19 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 public class ApiResponse<T> {
     private HttpStatus status;
     private String message;
     private String error;
+    @JsonFormat(pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime timestamp;
     private T data;
 
-    private ApiResponse() {
-        timestamp = LocalDateTime.now();
+    public ApiResponse(){
+        this.timestamp = LocalDateTime.now();
     }
-
     public ApiResponse(HttpStatus status) {
         this();
         this.status = status;
--- src/main/java/com/possible/backendtest/service/CardVerificationServiceImpl.java
+++ src/main/java/com/possible/backendtest/service/CardVerificationServiceImpl.java
@@ -10,71 +10,53 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class CardVerificationServiceImpl implements CardVerificationService {
-        private LinkedHashMap<String, Integer> countPayload = new LinkedHashMap<>();
+    private LinkedHashMap<String, Integer> trackCardNumberHit = new LinkedHashMap<>();
 
     @Override
-        public CardResponseDto validate(String cardNumber) {
-            try {
-                ResponseEntity<Card> responseEntity = new RestTemplate()
-                        .getForEntity("https://lookup.binlist.net/{cardNumber}", Card.class, cardNumber);
-                Card response = responseEntity.getBody();
-                assert response != null;
-                Payload payload = new Payload(response.getScheme(), response.getType(), response.getBank().getName());
-                addNumberOfHits(cardNumber);
-                return new CardResponseDto(true, payload);
-            } catch (Exception e) {
-                throw new CustomException(e.getMessage(), HttpStatus.BAD_REQUEST);
-            }
+    public CardResponseDto validate(String cardNumber) {
+        try {
+            ResponseEntity<Card> responseEntity = new RestTemplate().getForEntity("https://lookup.binlist.net/{cardNumber}", Card.class, cardNumber);
+            Card response = responseEntity.getBody();
+            assert response != null;
+            Payload payload = new Payload(response.getScheme(), response.getType(), response.getBank().getName());
+            addNumberOfHits(cardNumber);
+            return new CardResponseDto(true, payload);
+        } catch (Exception e) {
+            throw new CustomException(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+    }
 
 
-    protected void addNumberOfHits(String cardNumber) {
-            if (!countPayload.containsKey(cardNumber)) {
-                countPayload.put(cardNumber, 1);
-            } else {
-                int count = countPayload.get(cardNumber) + 1;
-                countPayload.put(cardNumber, count);
+    public void addNumberOfHits(String cardNumber) {
+            if (!trackCardNumberHit.containsKey(cardNumber)) {
+                trackCardNumberHit.put(cardNumber, 1);
             }
-        System.out.println(countPayload);
-        System.out.println(countPayload.size());
+            else {
+                trackCardNumberHit.put(cardNumber, trackCardNumberHit.get(cardNumber) + 1);
+            }
         }
 
 
     @Override
     public HitCountDto hitCount(int start, int limit) {
         try {
-            LinkedHashMap<String, Integer> payload = new LinkedHashMap<>();
-            if (countPayload.size() > 0) {
-                List<String> list = new ArrayList<>();
-                for (String key : countPayload.keySet()) {
-                    list.add(key);
-                }
-
-                 if (start > countPayload.size()) {
-                    throw new CustomException("Please reduce start number", HttpStatus.BAD_REQUEST);
-                }
-
-                else {
-                     int end;
-                     if ((start + limit) > countPayload.size()) {
-                         end = countPayload.size() + 1;
-                     } else {
-                         end = start + limit;
-                     }
-
-                     for (int i = start; i < end; i++) {
-                         payload.put(list.get(i - 1), countPayload.get(list.get(i - 1)));
-                     }
-                 }
+            if(start > trackCardNumberHit.size()){
+                throw new CustomException("Please reduce start number", HttpStatus.BAD_REQUEST);
             }
-            return new HitCountDto(true, start, limit, countPayload.size(), payload);
+            else {
+                LinkedHashMap<String, Integer> payload = trackCardNumberHit.entrySet()
+                        .stream()
+                        .limit(limit)
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (key, value) -> value, LinkedHashMap::new));
+
+                return new HitCountDto(true, start, limit, trackCardNumberHit.size(), payload);
+            }
         } catch (Exception e) {
             throw new CustomException(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
